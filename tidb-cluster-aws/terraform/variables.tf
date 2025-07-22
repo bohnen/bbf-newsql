33b8fa3c8d8dde34ad58fb80dafafa66f2ee9730
bohnen <bohnen.net@gmail.com>
2025-07-22 11:08:30 +0900

    changed to use 192.168.x.x

--- tidb-cluster-aws/terraform/variables.tf
+++ tidb-cluster-aws/terraform/variables.tf
@@ -68,20 +68,20 @@ variable "monitor_instance_type" {
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "192.168.0.0/16"
 }
 
 # サブネットCIDR
 variable "public_subnet_cidr" {
   description = "CIDR block for public subnet"
   type        = string
-  default     = "10.0.1.0/24"
+  default     = "192.168.1.0/24"
 }
 
 variable "private_subnet_cidr" {
   description = "CIDR block for private subnet"
   type        = string
-  default     = "10.0.2.0/24"
+  default     = "192.168.2.0/24"
 }
 
 # AMI ID (Ubuntu 22.04 LTS)
